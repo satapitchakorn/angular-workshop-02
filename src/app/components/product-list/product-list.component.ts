c9d6e4e67d951c62a297fa80109d493b1c21023a
Satapitchakorn Punjarat <satapitchakorn.punjarat@allianz.com>
2020-07-21 16:40:52 +0700

    Change pipe number & rebuild to github pages.

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../../models/product'
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-list',
@@ -12,14 +12,14 @@ export class ProductListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.generate()
+    this.generate();
   }
-  generate() {
-    const p1 = new Product('C01', 'Product01', 'https://.com', 10.0, '*', true)
-    const p2 = new Product('C02', 'Product01', 'https://.com', 10.0, '**', true)
-    const p3 = new Product('C03', 'Product01', 'https://.com', 10.0, '***', true)
-    const p4 = new Product('C04', 'Product01', 'https://.com', 10.0, '****', true)
-    const p5 = new Product('C05', 'Product01', 'https://.com', 10.0, '*****', true)
+  generate(): void {
+    const p1 = new Product('C01', 'Product01', 'https://image.com', 10.0, '*', true);
+    const p2 = new Product('C02', 'Product01', 'https://image.com', 10.750, '**', true);
+    const p3 = new Product('C03', 'Product01', 'https://image.com', 10.0, '***', true);
+    const p4 = new Product('C04', 'Product01', 'https://image.com', 10.0, '****', true);
+    const p5 = new Product('C05', 'Product01', 'https://image.com', 10.0, '*****', true);
 
     this.productList.push(p1, p2, p3, p4, p5)
   }
